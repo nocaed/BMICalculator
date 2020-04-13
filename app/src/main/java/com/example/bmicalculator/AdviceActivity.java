cef4f1750e6a01ef009d81c4f19ff0ab85cf3256
nocaed <codenamedeacon@outlook.com>
2020-04-13 14:24:14 -0400

    initialized

--- app/src/main/java/com/example/bmicalculator/AdviceActivity.java
+++ app/src/main/java/com/example/bmicalculator/AdviceActivity.java
@@ -10,5 +10,6 @@ public class AdviceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice);
+        setTitle("Advice based on the BMI");
     }
 }
